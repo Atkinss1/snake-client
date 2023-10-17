6cbdc11e8e4117bfc1690c7ebe97dd49bd747cb9
Atkinss1 <jordanatkins@live.com>
2023-10-16 21:04:59 -0600

    refactor: moved connect function to new file, import connect function

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
-const net = require('net');
+const client = require('./client');
 
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-
-  conn.on('data', (data) => {
-    console.log("Welcome to Snek!", data);
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  return conn;
-};
+client.connect();
 
 console.log('connecting ...');
-connect();
