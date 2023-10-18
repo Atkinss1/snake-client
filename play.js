56d7573b3736a131819126b494e65fb540314a8d
Atkinss1 <jordanatkins@live.com>
2023-10-17 21:30:02 -0600

    refactor: moved setupInput and handleUserInput functions into input.js

--- play.js
+++ play.js
@@ -1,21 +1,7 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('connecting ...');
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf-8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
 
 connect();
 setupInput();
