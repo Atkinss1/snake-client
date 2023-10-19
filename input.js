83588f0bef6481c930356ea3cce7bd3fed0cb445
Atkinss1 <jordanatkins@live.com>
2023-10-18 21:15:50 -0600

    add: setInterval to allow the snake to move continously

--- input.js
+++ input.js
@@ -1,8 +1,9 @@
+const { stdin } = require('./constants');
 let connection;
+
 
 const setupInput = function(conn) {
   connection = conn;
-  const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf-8');
   stdin.resume();
@@ -10,27 +11,47 @@ const setupInput = function(conn) {
   return stdin;
 };
 
+// define variable to store intervalId
+let intervalId;
+
+// comparing user keystrokes
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
+  if (key === '\u0003' || key === 'q') {
     process.exit();
   }
+  // clear previous setInterval before starting new setInterval
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+
   if (key === 'w') {
-    connection.write('Move: up');
+    intervalId = setInterval(() => {
+      connection.write('Move: up');
+    }, 200);
   }
   if (key === 's') {
-    connection.write('Move: down');
+    intervalId = setInterval(() => {
+      connection.write('Move: down');
+    }, 200);
   }
   if (key === 'a') {
-    connection.write('Move: left');
+    intervalId = setInterval(() => {
+      connection.write('Move: left');
+    }, 200);
   }
   if (key === 'd') {
-    connection.write('Move: right');
+    intervalId = setInterval(() => {
+      connection.write('Move: right');
+    }, 200);
   }
   if (key === '1') {
     connection.write('Say: hello');
   }
   if (key === '2') {
     connection.write('Say: HA-HA!');
+  }
+  if (key === '3') {
+    connection.write('Say: oof, that hurt!');
   }
   
   
