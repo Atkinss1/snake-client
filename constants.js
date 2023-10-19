c5bde3abd50033a94a6fdf2762c6d258a5e46f00
Atkinss1 <jordanatkins@live.com>
2023-10-18 21:17:01 -0600

    add: welcomeMessage and stdin constants

--- constants.js
+++ constants.js
@@ -1,8 +1,12 @@
 const IP = 'localhost';
 const PORT = 50541;
+const welcomeMessage = 'You have sucessfully connected to the game server!';
+const stdin = process.stdin;
 
 module.exports = {
   IP,
-  PORT
+  PORT,
+  welcomeMessage,
+  stdin
 };
 
