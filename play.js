ba20eb10c0b103f7fcdd511dd4ce2c7c77afd093
Atkinss1 <jordanatkins@live.com>
2023-10-18 19:42:58 -0600

    add: comment on function call

--- play.js
+++ play.js
@@ -3,4 +3,5 @@ const { setupInput } = require('./input');
 
 console.log('connecting ...');
 
+// setupInput will configure userinput by taking in the object returned from connect() and return the user input to the server through connect
 setupInput(connect());
