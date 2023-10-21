cb568c4a305b7e8b85a105fd9afae2f6bdb7073b
Atkinss1 <jordanatkins@live.com>
2023-10-21 16:53:38 -0600

    modularized userInput and exported for use in input.js

--- constants.js
+++ constants.js
@@ -3,10 +3,21 @@ const PORT = 50541;
 const welcomeMessage = 'You have sucessfully connected to the game server!';
 const stdin = process.stdin;
 
+const userInput = {
+  'w': 'Move: up',
+  's': 'Move: down',
+  'a': 'Move: left',
+  'd': 'Move: right',
+  '1': 'Say: hello',
+  '2': 'Say: HA-HA!',
+  '3': 'Say: oof, that hurt!'
+};
+
 module.exports = {
   IP,
   PORT,
   welcomeMessage,
-  stdin
+  stdin,
+  userInput
 };
 
